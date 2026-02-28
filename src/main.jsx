c54569233b63393f50f5133f18c4b177983f15a9
Thiago de Melo Torres Lopes <thiagodemelo2001@hotmail.com>
2026-02-27 22:46:09 -0300

    Aula 27/02

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import './estilos/reset.css'
+import './estilos/globals.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,11 @@
+export default function Sobre() {
+    return (
+        <>
+            <h1>Sobre</h1>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Distinctio quisquam repellat repudiandae libero delectus ipsa, amet tenetur maxime ad culpa optio recusandae,
+                nulla odit architecto, impedit odio at neque reiciendis.</p>
+        </>
+    )
+}
